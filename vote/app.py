24a6c2dea99ae2da74f6a14adede09e6b98fd5f5
Seno Aji <cydnirn.dev@gmail.com>
2025-06-21 15:55:17 +0700

    Add ECS task definitions for vote and result services
    Enable SSL for Redis in vote app
    
    Update deploy workflow with new ECS services and task definitions

--- vote/app.py
+++ vote/app.py
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="master.vote-redis.arfzqw.use1.cache.amazonaws.com", port=6582, db=0, socket_timeout=5)
+        g.redis = Redis(host="master.vote-redis.arfzqw.use1.cache.amazonaws.com", port=6582, db=0, socket_timeout=5,ssl=True)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
